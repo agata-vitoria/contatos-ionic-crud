ccd0cb0641cd487c3014ea2e993d59efab44bfb4
agata-vitoria <agatavitoriadelima@gmail.com>
2020-05-19 22:30:26 -0300

    CRUD ionic consumindo API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contato } from 'src/modelos/contato';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public contatos: Contato[];
+  public newContato: Contato = <Contato>{};
+  public resp: any;
+  public url: string;
 
-  constructor() {}
+  constructor(public http: HttpClient) {
+    this.url = 'https://mobetec.000webhostapp.com/ContatoApi/v1/Api.php?apicall=';
+    this.lerContato();
+  }
 
+  lerContato() {
+    this.http.get(this.url + 'getcontatos')
+      .subscribe((response) => {
+        this.resp = response;
+        this.contatos = this.resp.contatos;
+      });
+  }
+
+  incluirContato() {
+    let param = new HttpParams()
+      .set('nome', String(this.newContato.nome))
+      .set('telefone', String(this.newContato.telefone))
+      .set('nota', String(this.newContato.nota))
+      .set('tipo', String(this.newContato.tipo));
+
+    this.http.post(this.url + 'createcontato', param)
+      .subscribe((response) => {
+        this.lerContato();
+      })
+  }
+
+  deletarContato(contatosel: Contato) {
+    this.http.get(this.url + 'deletecontato&id=' + contatosel.id)
+    .subscribe((response) => {
+      this.lerContato();
+    })
+  }
+
+  atualizarContato(contatosel: Contato) {
+    let param = new HttpParams()
+    .set('id', contatosel.id.toString())
+    .set('nome', String(this.newContato.nome))
+    .set('telefone', String(this.newContato.telefone))
+    .set('nota', String(this.newContato.nota))
+    .set('tipo', String(this.newContato.tipo));
+
+    this.http.post(this.url + 'updatecontato', param).subscribe((response) => {
+      this.lerContato();
+    })
+  }
 }
+
+
+
---
+++ src/modelos/contato.ts
@@ -0,0 +1,7 @@
+export interface Contato {
+    id: number;
+    nome: String;
+    telefone: String;
+    nota: number;
+    tipo: number;
+}
